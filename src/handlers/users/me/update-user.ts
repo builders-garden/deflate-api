d4ed038e03a2ebe41be977d1e4bd4a49a5195a9c
lim0n3 <simonestaffa96@gmail.com>
2024-11-16 15:09:41 +0700

    add referrer to api

--- src/handlers/users/me/update-user.ts
+++ src/handlers/users/me/update-user.ts
@@ -4,11 +4,12 @@ import { snakeToCamelCase } from "../../../utils";
 
 export const updateUser = async (req: Request, res: Response) => {
   const user = req.user!;
-  const { username, mode } = req.body;
+  const { username, mode, referrer } = req.body;
   await setCustomMetadata(user.id, {
     ...(user.customMetadata ?? {}),
     username,
     mode,
+    referrer,
   });
   return res.json(snakeToCamelCase(user));
 };
