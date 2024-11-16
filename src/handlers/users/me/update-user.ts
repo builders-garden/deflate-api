76611af5266ec2cb0fce2167424b271c463e6409
lim0n3 <simonestaffa96@gmail.com>
2024-11-16 15:14:08 +0700

    allow question saving

--- src/handlers/users/me/update-user.ts
+++ src/handlers/users/me/update-user.ts
@@ -4,12 +4,15 @@ import { snakeToCamelCase } from "../../../utils";
 
 export const updateUser = async (req: Request, res: Response) => {
   const user = req.user!;
-  const { username, mode, referrer } = req.body;
+  const { username, mode, referrer, question1, question2, question3 } = req.body;
   await setCustomMetadata(user.id, {
     ...(user.customMetadata ?? {}),
     username,
     mode,
     referrer,
+    question1,
+    question2,
+    question3,
   });
   return res.json(snakeToCamelCase(user));
 };
